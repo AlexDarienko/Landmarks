269176ce06a7be72530e8f59aadce39d4a32401a
Alex Sander <alex.darienko@gmail.com>
2025-02-23 07:50:03 +0200

    Creating and combining views

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Alex Sander on 23.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
