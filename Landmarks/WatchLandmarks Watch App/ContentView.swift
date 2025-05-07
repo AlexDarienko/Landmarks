58056119b89db0ff7634e4a96c8f80f10271b436
Alex Sander <alex.darienko@gmail.com>
2025-05-07 11:41:54 +0300

    Creating a watchOS app

---
+++ Landmarks/WatchLandmarks Watch App/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Alex Sander on 07.05.2025.
+//
+
+import SwiftUI
+import UserNotifications
+
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .task {
+                let center = UNUserNotificationCenter.current()
+                _ = try? await center.requestAuthorization(
+                    options: [.alert, .sound, .badge]
+                )
+            }
+    }
+}
+
+
+#Preview {
+    ContentView()
+        .environment(ModelData())
+}
